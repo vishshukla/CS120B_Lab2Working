eacbd6b9625d015062c16aced0ad0e498992e546
Vishwas Shukla <vshuk003@wch136-16.cs.ucr.edu>
2020-01-12 22:08:06 -0800

    working

---
+++ source/main.c
@@ -0,0 +1,32 @@
+/*	Author: vshuk003
+ *  Partner(s) Name: Nicole Nguyen
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif	
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	unsigned char tmpB = 0x00;
+	unsigned char tmpA = 0x00;	
+	while(1) {
+		tmpA = PINA & 0x01;
+
+		if (tmpA == 0x01) {
+			tmpB = (tmpB & 0xFC) | 0x01;
+		} else {
+			tmpB = (tmpB & 0xFC) | 0x02;
+		}
+		PORTB = tmpB;
+	}
+	return 0;
+}
